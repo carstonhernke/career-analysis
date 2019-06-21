baf420a6697cb2405cb2105a09f8a36389a6b926
Carston Hernke <carston17@gmail.com>
2019-06-21 01:03:25 -0500

    Update data_processing.R

--- data_processing.R
+++ data_processing.R
@@ -2,7 +2,7 @@
 library(readr)
 library(dplyr)
 library(stringr)
-inc5000_2018 <- read_csv("~/Google Drive/Google Drive Personal/College/Resume/Company-Specific/Slalom/Slalom_Data_Challenge/inc5000-2018.csv", 
+inc5000_2018 <- read_csv("inc5000-2018.csv", 
                          col_types = cols(`_ - partner_lists - partner_lists` = col_skip()))
 colnames(inc5000_2018) <- c("ID","URL","Rank","City","Metro_Code","Industry_Code","Growth","Workers","Company","Website","State Long","State Short", "Revenue", "ZIP", "Founded", "Industry", "Latitude", "Metro_Code_Str", "Longitude", "Years_on_list", "Previous_Workers", "Metro_Name")
 
@@ -18,18 +18,18 @@ sum(inc5000_data$Revenue)
 
 sum(inc5000_data$Workers)
   # import income data by NAICS code
-NAICS_data <- read_csv("~/Google Drive/Google Drive Personal/College/Resume/Company-Specific/Slalom/Slalom_Data_Challenge/NAICS_data.csv")
+NAICS_data <- read_csv("NAICS_data.csv")
 NAICS_data$`Top Level Code` <- as.character(NAICS_data$`Top Level Code`)
 
 # import NAICS lookup table
-NAICS_lookup <- read_csv("~/Google Drive/Google Drive Personal/College/Resume/Company-Specific/Slalom/Slalom_Data_Challenge/NAICS_lookup.csv", 
+NAICS_lookup <- read_csv("NAICS_lookup.csv", 
                          col_types = cols(NAICS_Code = col_character()))
 joined_data <- inc5000_2018 %>% 
   left_join(NAICS_lookup, by = c("Industry" = "INC_5000_Industry")) %>%
   left_join(NAICS_data, by = c("NAICS_Code" = "Top Level Code"))
 
 # add data about household rents
-housing_costs <- read_csv("~/Google Drive/Google Drive Personal/College/Resume/Company-Specific/Slalom/Slalom_Data_Challenge/ACS_ZIP.csv", 
+housing_costs <- read_csv("ACS_ZIP.csv", 
                           col_types = cols(Geography = col_character()))
 
 housing_costs <- housing_costs %>% select(Geography,'Estimate; Median gross rent -- - Total: - 1 bedroom') %>% rename(One_Bed_Rent = 'Estimate; Median gross rent -- - Total: - 1 bedroom', ZIP = Geography) %>% mutate(ZIP = substr(ZIP, 7, 12))
